6004e1495a87ab66dd0c593e3d8fd0a928113f9e
arivm7 <arivm@yandex.ru>
2025-05-25 23:01:19 +0300

    Добавление скрипта для бакапа с ротацией С добавлением в инсталлятор

--- sync_install.sh
+++ sync_install.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env bash
-set -e
-set -u
-set -o pipefail
+set -euo pipefail
 
 
 
 APP_TITLE="Инсталятор персонального синхронизатора sync_1"
-VERSION="1.3.0 (2025-04-29)"
+VERSION="1.3.1 (2025-05-25)"
 APP_NAME=$(basename "$0")
 LAST_CHANGES="\
-v1.3.0 (2025-04-29): Добавление установки зависимостей."
+v1.3.0 (2025-04-29): Добавление установки зависимостей.
+v1.3.1 (2025-05-25): Переделывание установки зависимостей
+"
 
 echo "SYNC INSTALLER VER: ${VERSION}"
 
 
 
-APP_SYNC="rsync";    PKG_SYNC="rsync";
-APP_SSH="ssh";       PKG_SSH="openssh-client";
-APP_FIGLET="figlet"; PKG_FIGLET="figlet";
+# Обязательные зависимости
+# shellcheck disable=SC2034
+declare -A DEPENDENCIES_REQUIRED=(
+    ["rsync"]="rsync"
+    ["ssh"]="openssh-client"
+    ["figlet"]="figlet"
+    ["tar"]="tar"
+    ["du"]="coreutils"
+    ["df"]="coreutils"
+    ["awk"]="gawk"
+    ["gzip"]="gzip"
+)
+
+# Рекомендованные зависимости
+# shellcheck disable=SC2034
+declare -A DEPENDENCIES_OPTIONAL=(
+    ["pv"]="pv"
+    ["realpath"]="coreutils"
+    ["readlink"]="coreutils"
+)
 
 
 
@@ -37,13 +54,81 @@ print_help()
 
 
 
+# Проверка наличия команды в системе
+is_installed() {
+    command -v "$1" &>/dev/null
+}
+
+# Проверка доступности пакета в APT
+is_available_in_repo() {
+    apt-cache show "$1" &>/dev/null
+}
+
+
+#
+#  Проверка и установка зависимости
+#   check_dependency_group <массив> [0|1]
+#   <массив> -- Ассоциативный массив, где 
+#               ключ -- программа, значение -- пакет.
+#   [0|1]    -- 0 -- не оязательные зависимости
+#               1 -- обязательные зависимости. По умолчани.
+#   После проверки при отсутсвии предлагает установить программу.
+#   При подтверждении -- устанавливает.
+#
+check_dependency_group() {
+    local -n dep_array=${1:?}    # ссылка на ассоциативный массив
+    local is_required=${2:-1}    # 1 - обязательная, 0 - необязательная
+
+    for cmd in "${!dep_array[@]}"; do
+        local pkg="${dep_array[$cmd]}"
+
+        if is_installed "$cmd"; then
+            echo "[OK] Утилита '$cmd' установлена."
+        else
+            echo "[!!] Утилита '$cmd' не найдена. Пакет: '$pkg'"
+
+            if [[ "$is_required" == "1" ]]; then 
+                echo "(пакет обязательный)"
+            else 
+                echo "(пакет рекомендуемый, не обязатеьный)"
+            fi
+
+            if is_available_in_repo "$pkg"; then
+                echo -n "Желаете установить '$pkg'? [Yes/n]: "
+                read -r answer
+                answer="${answer,,}"  # в нижний регистр
+                if [[ "$answer" =~ ^(y|yes|)$ ]]; then
+                    sudo apt update && sudo apt install -y "$pkg"
+                    if is_installed "$cmd"; then
+                        echo "[OK] '$cmd' успешно установлен."
+                    else
+                        echo "[Ошибка] Не удалось установить '$cmd'."
+                        [[ "$is_required" == "1" ]] && exit 1
+                    fi
+                else
+                    echo "Вы отказались от установки '$pkg'."
+                    if [[ "$is_required" == "1" ]]; then
+                        echo "Это обязательная зависимость. Прерывание."
+                        exit 1
+                    fi
+                fi
+            else
+                echo "[Ошибка] Пакет '$pkg' не найден в репозиториях."
+                [[ "$is_required" == "1" ]] && exit 1
+            fi
+        fi
+    done
+}
+
+
+
 #
 # Проверяет установлена ли программа. 
 # Если нет, то устанавливает пакет, в котором она находится
 # $1 -- Программа
 # $2 -- Пакет, в котором эта программа. Для установки программы.
 #
-install_if_not()
+install_if_not_()
 {
     APP="$1"
     PKG="$2"
@@ -86,10 +171,8 @@ then
 fi
 
 
-
-install_if_not "${APP_SYNC}"   "${PKG_SYNC}"
-install_if_not "${APP_SSH}"    "${PKG_SSH}"
-install_if_not "${APP_FIGLET}" "${PKG_FIGLET}"
+check_dependency_group DEPENDENCIES_REQUIRED 1
+check_dependency_group DEPENDENCIES_OPTIONAL 0
 
 
 
@@ -99,6 +182,7 @@ scripts_files=(
 sync_1.sh
 sync_all.sh
 sync_1_aliases.sh
+sync_backuper.sh
 )
 # папка назначения для копирования скриптов
 scripts_to="${HOME}/bin"
@@ -118,7 +202,7 @@ icon_to="${HOME}/bin/icons"
 
 # Конфиг файл для массовой синхронизации
 SYNC_ALL_LIST_FILE="sync_all.list"
-
+SYNC_BACKUPER_LIST="sync_backuper.list"
 
 
 # Список имен файлов .desktop для копирования
@@ -168,29 +252,39 @@ copy_file_to desktop_files "${desktop_to}"
 
 
 
-echo ""
-echo "Устанавливаем конфиг-файл для SYNC_ALL [${SYNC_ALL_LIST_FILE}]"
+#
+#  Устанавлвивает конфиг файл, если его нет.
+# Если есть. то сообщает об этом и ничего не делает
+# install_config_file <путь_установки> <файл>
+#
+install_config_file() {
+    local target_dir="${1:?}"       # путь назначения
+    local config_file="${2:?}"      # имя конфиг-файла
+    echo ""
+    echo "Устанавливаем конфиг-файл [${config_file}]"
 
-if [ -f "${SYNC_ALL_LIST_FILE}" ]; then
-    echo "Дефолтный конфиг для списка синхронизации есть"
-    if [ -f "${scripts_to}/${SYNC_ALL_LIST_FILE}" ]; then
-        echo "Установленный конфиг для списка синхронизации есть."
-        echo "Если Вам нужно установить дефолтный конфиг, "
-        echo "то удалите уже установленный конфиг-файл [${scripts_to}/${SYNC_ALL_LIST_FILE}]"
-        echo "Оставляем существующий конфиг-файл [${SYNC_ALL_LIST_FILE}]."
+    if [ -f "${config_file}" ]; then
+        echo "Дефолтный конфиг есть"
+        if [ -f "${target_dir}/${config_file}" ]; then
+            echo "Установленный конфиг есть."
+            echo "Если Вам нужно установить дефолтный конфиг, "
+            echo "то удалите уже установленный конфиг-файл [${target_dir}/${config_file}]"
+            echo "Оставляем существующий конфиг-файл [${config_file}]."
+        else
+            printf "==== Копируем файл %s -> %s\n" "${config_file}" "${target_dir}/${config_file}"
+            cp --force "${config_file}" "${target_dir}/${config_file}"
+            echo "Дефолтный конфиг-файл [${config_file}] установлен."
+        fi
     else
-        printf "==== Копируем файл %s -> %s\n" "${SYNC_ALL_LIST_FILE}" "${scripts_to}/${SYNC_ALL_LIST_FILE}"
-        cp --force "${SYNC_ALL_LIST_FILE}" "${scripts_to}/${SYNC_ALL_LIST_FILE}"
-        echo "Дефолтный конфиг-файл [${SYNC_ALL_LIST_FILE}] установлен."
+        echo "Файл дефолтного конфига отсутствует [${config_file}]."
+        echo "Аварийное прекращение работы."
+        exit 1
     fi
+    echo ""
+}
 
-else
-    echo "Дефолтный конфиг для списка синхронизации отсутствует [${SYNC_ALL_LIST_FILE}]."
-    echo "Аварийное прекращение работы."
-    exit 1;
-fi
-echo ""
-
+install_config_file "${scripts_to}" "${SYNC_ALL_LIST_FILE}"
+install_config_file "${scripts_to}" "${SYNC_BACKUPER_LIST}"
 
 
 echo ""
